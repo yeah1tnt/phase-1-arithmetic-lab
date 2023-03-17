588cf1906f42226d08b626bebc8098a23ee550a4
Tong Tang <pichip22@hotmail.com>
2023-03-17 17:30:12 -0400

    random math

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 64;
+let num4 = 6;
+let multiply = num1*num2;
+let random = Math.floor(Math.random()*10) + 1;
+let mod = 64%6;
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
