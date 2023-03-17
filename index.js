70f2b4802ecb389014e3c7afb7a3b6c7c6ea036f
Tong Tang <pichip22@hotmail.com>
2023-03-17 17:32:04 -0400

    change random

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let num2 = 31;
 let num3 = 64;
 let num4 = 6;
 let multiply = num1*num2;
-let random = Math.floor(Math.random()*10) + 1;
+let random = Math.round(Math.random()*10) ;
 let mod = 64%6;
 let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
